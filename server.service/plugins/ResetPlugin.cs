8d8fad07aee46448ae72657381086063d3816faa
wwping <1069410172@qq.com>
2021-08-24 15:56:15 +0800

    update

---
+++ server.service/plugins/ResetPlugin.cs
@@ -0,0 +1,58 @@
+﻿using common.cache;
+using common.extends;
+using server.model;
+using server.plugin;
+using server.service.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.service.plugins
+{
+    public class ResetPlugin : IPlugin
+    {
+        public MessageTypes MsgType => MessageTypes.SERVER_RESET;
+
+        public void Excute(PluginExcuteModel data, ServerType serverType)
+        {
+            MessageResetModel model = data.Packet.Chunk.ProtobufDeserialize<MessageResetModel>();
+
+            //A已注册
+            RegisterCacheModel source = ClientRegisterCache.Instance.Get(model.Id);
+            if (source != null)
+            {
+                //B已注册
+                RegisterCacheModel target = ClientRegisterCache.Instance.Get(model.ToId);
+                if (target != null)
+                {
+                    //是否在同一个组
+                    if (source.GroupId != target.GroupId)
+                    {
+                        return;
+                    }
+
+                    if (serverType == ServerType.UDP)
+                    {
+                        UDPServer.Instance.Send(new MessageRecvQueueModel<IMessageModelBase>
+                        {
+                            Address = data.SourcePoint,
+                            TcpCoket = null,
+                            Data = model
+                        });
+                    }
+                    else if (serverType == ServerType.TCP)
+                    {
+                        TCPServer.Instance.Send(new MessageRecvQueueModel<IMessageModelBase>
+                        {
+                            Address = data.SourcePoint,
+                            TcpCoket = data.TcpSocket,
+                            Data = model
+                        });
+                    }
+                }
+            }
+        }
+    }
+}
